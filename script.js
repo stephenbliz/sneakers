c0c8422855c095befe678505b5456eaa5a0bb124
stephenbliz <uzomaebuka11@gmail.com>
2022-10-29 13:17:58 +0100

    Added javascript on chosing the quantity of products and adding the items to cart and their total amount

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-// dropdown menu 
+//  Querying the DOM
 
 const menu = document.querySelector('.hidden');
 const dropDown = document.querySelector('.mobile-menu-bckg');
 const closeMenu = document.querySelector('.close');
+const cartIndicator = document.querySelector('.num-of-prd');
+const minusIcon = document.querySelector('.minus');
+const plusIcon = document.querySelector('.plus');
+const display = document.querySelector('.icon-p');
+const cartIcon = document.querySelector('.cart');
+const cartContent = document.querySelector('.cart-dropdown');
+const selectedItems = document.querySelector('.cart-dropdown-content');
+const price = document.querySelector('.prices')
+const addToCart = document.querySelector('.cart-icon');
+console.log(selectedItems);
+
+// dropdown menu
 
 menu.addEventListener('click', () =>{
     dropDown.classList.toggle('hidden2');
@@ -14,24 +26,45 @@ closeMenu.addEventListener('click', () => {
 
 // Dropdown Cart
 
-const cartIcon = document.querySelector('.cart');
-const cartContent = document.querySelector('.cart-dropdown');
-
 cartIcon.addEventListener('click', () =>{
     cartContent.classList.toggle('hidden3');
+
 });
 
 // Chosing the quantity of products
 
-const minusIcon = document.querySelector('.minus');
-const plusIcon = document.querySelector('.plus');
-const display = document.querySelector('.icon-p');
-
-// let increase = '1';
 plusIcon.addEventListener('click', () => {
     display.innerText++
 });
 
 minusIcon.addEventListener('click', () => {
-    display.innerText--
+     if(display.innerText !== '0'){
+        display.innerText--;
+     }else{
+        return
+     };
+});
+
+// Adding the number of items to cart 
+
+addToCart.addEventListener('click', () =>{
+    cartIndicator.style.display ='block';
+    cartIndicator.innerText = display.innerText;
+    const shoePrice = 125.45;
+    const totalAmount = shoePrice * Number(cartIndicator.innerText);
+
+    if(cartIndicator.innerText !== '0'){
+        selectedItems.innerHTML = `
+<img class="jsImage" src="./images/image-product-1-thumbnail.jpg" alt="">
+<div class="jsDiv">
+    <p class="jsDiv-p1">fall limited edition sneakers</p>
+    <p class="jsDiv-p2">$${shoePrice} x ${cartIndicator.innerText}</p>
+    <p class="jsDiv-p2 amount">$${totalAmount}</p>
+</div>
+<img class="delete-icon" src="./images/icon-delete.svg" alt="delete-icon">
+<div class="check-out">checkout</div>
+`;
+    }else{
+        return
+    };
 });
